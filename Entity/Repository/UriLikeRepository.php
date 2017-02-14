a057785439a843b3cad441a9c3cfc93aa083c7be
Eluminae <eddie.barraco@idci-consulting.fr>
2017-02-14 10:07:45 +0100

    Add UriLike entity.

---
+++ Entity/Repository/UriLikeRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tms\Bundle\LikeBundle\Entity\Repository;
+
+use Tms\Bundle\RestBundle\Repository\AbstractEntityRepository;
+
+/**
+ * ConfigurationBenefitCategoryRepository
+ */
+class UriLikeRepository extends AbstractEntityRepository
+{
+}
---
+++ Entity/UriLike.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Tms\Bundle\LikeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * UriLike
+ *
+ * @ORM\Table(
+ *     name="uri_like",
+ *     uniqueConstraints={@ORM\UniqueConstraint(name="uri_like_unique", columns={"uri_like", "user_id"})}
+ * )
+ * @ORM\Entity(repositoryClass="Tms\Bundle\LikeBundle\Entity\Repository\UriLikeRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class UriLike
+{
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", name="uri_like")
+     */
+    private $uriLike;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", name="user_id")
+     */
+    private $userId;
+
+    /**
+     * @var \datetime
+     *
+     * @ORM\Column(type="datetime", name="created_at")
+     */
+    private $createdAt;
+
+    /**
+     * Constructor
+     */
+    public function __construct($uriLike, $userId)
+    {
+        $this->setUriLike($uriLike);
+        $this->setUserId($userId);
+        $this->createdAt = new \datetime('now');
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set uriLike
+     *
+     * @param string $uriLike
+     *
+     * @return UriLike
+     */
+    public function setUriLike($uriLike)
+    {
+        $this->uriLike = $uriLike;
+
+        return $this;
+    }
+
+    /**
+     * Get uriLike
+     *
+     * @return string
+     */
+    public function getUriLike()
+    {
+        return $this->uriLike;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param string $userId
+     *
+     * @return UriLike
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return string
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
